d5d6b4d0d14375cc28b1561683885cd0b334b206
gatlee21 <atleegraham16@gmail.com>
2025-07-19 14:53:38 -0400

    Add state and handleChange callback

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import InputField from '../components/InputField.jsx';
 
 function Calculator() {
+
+  const [inputs, setInputs] = useState({
+    focalLength: 66,
+    lensToDisplay: 65,
+    displayWidth: 121,
+    displayHeight: 64,
+    eyeRelief: 18,
+    ipd: 70,
+  });
+
+  function handleChange(e) {
+    setInputs({ ...inputs, [e.target.name]: Number(e.target.value) });
+  }
+
   return (
     <div>
-      <p>Enter your display size and eye-to-lens distance!</p>
+
     </div>
   );
 }
