4b7345811a063093a6a6f1c427e2250a904c9b3f
gatlee21 <atleegraham16@gmail.com>
2025-07-19 14:56:40 -0400

    add InputFields to calculator

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -18,7 +18,48 @@ function Calculator() {
 
   return (
     <div>
-
+      <InputField
+        label="Focal length"
+        value={inputs.focalLength}
+        onChange={handleChange}
+        name="focalLength"
+        unit="mm"
+      />
+      <InputField
+        label="Lens-to-display distance"
+        value={inputs.lensToDisplay}
+        onChange={handleChange}
+        name="lensToDisplay"
+        unit="mm"
+      />
+      <InputField
+        label="Display width"
+        value={inputs.displayWidth}
+        onChange={handleChange}
+        name="displayWidth"
+        unit="mm"
+      />
+      <InputField
+        label="Display height"
+        value={inputs.displayHeight}
+        onChange={handleChange}
+        name="displayHeight"
+        unit="mm"
+      />
+      <InputField
+        label="Eye-relief"
+        value={inputs.eyeRelief}
+        onChange={handleChange}
+        name="eyeRelief"
+        unit="mm"
+      />
+      <InputField
+        label="Interpupillary distance (IPD)"
+        value={inputs.ipd}
+        onChange={handleChange}
+        name="ipd"
+        unit="mm"
+      />
     </div>
   );
 }
